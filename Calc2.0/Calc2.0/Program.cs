5ec4b97874d98f25abc9d32bdf762d990a2f73b2
Rafa Cruz <jose.cruz23@ulatina.net>
2025-11-06 23:27:01 -0600

    Calculadora Mejorada

---
+++ Calc2.0/Calc2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calc2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Mejorada");
+        }
+    }
+}
